467d7287209a16913328a86f4fbb88c617ff0054
Murillo Deolino <bye456oi@gmail.com>
2025-01-27 00:17:50 -0300

    adding some examples for community testing case

---
+++ examples/simple-use/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/Murilinho145SG/gouter/gouter"
+	"github.com/Murilinho145SG/gouter/gouter/httpio"
+)
+
+func main() {
+	/*
+		Statement to start the router to be able to read the routes
+	*/
+	router := gouter.NewRouter(false)
+	router.Route("/example", func(w httpio.Writer, r *httpio.Request) {
+		w.Add("Access-Control-Allow-Methods", "GET, OPTIONS")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(200)
+			return
+		}
+
+		if r.Method != "GET" {
+			w.WriteHeader(405)
+			return
+		}
+
+		// For reasons of good practice and code structuring, write the header before giving a response.
+		w.WriteHeader(200)
+		w.Write([]byte("Hello World!"))
+	})
+
+	/*
+		The connection initiator to be able to receive requests
+	*/
+	gouter.Run(":8080", router)
+}
